efb7599162d41c847a905b855c33884f79dac7db
Tony Benoy <me@tonybenoy.com>
2020-08-16 14:30:06 +0530

    migrations fin

--- src/migrations/alembic_config/versions/base_migration.py
+++ src/migrations/alembic_config/versions/base_migration.py
@@ -96,6 +96,53 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
     )
 
+    op.create_table(
+        "order",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("order_id", sa.Integer(), nullable=False),
+        sa.Column("merchant_id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("status", sa.String(), nullable=False),
+        sa.Column("total", sa.Numeric(), nullable=False),
+        sa.Column("tax", sa.Numeric(), nullable=False),
+        sa.Column("gross", sa.Numeric(), nullable=False),
+        sa.Column("discount", sa.Numeric(), nullable=False),
+        sa.Column(
+            "extra_details",
+            sa.JSON(),
+            autoincrement=False,
+            nullable=False,
+            server_default=sa.text("'{}'"),
+        ),
+        sa.Column("created_at", sa.TIMESTAMP(), nullable=False),
+        sa.Column("updated_at", sa.TIMESTAMP(), nullable=False),
+        sa.Column("row_status", sa.String(), nullable=False),
+        sa.Column("performed_by", sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+    op.create_table(
+        "order_lines",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("order_id", sa.Integer(), nullable=False),
+        sa.Column("product_id", sa.Integer(), nullable=False),
+        sa.Column("quantity", sa.Numeric(), nullable=False),
+        sa.Column("status", sa.String(), nullable=False),
+        sa.Column("discount", sa.Numeric(), nullable=False),
+        sa.Column("price", sa.Numeric(), nullable=False),
+        sa.Column(
+            "extra_details",
+            sa.JSON(),
+            autoincrement=False,
+            nullable=False,
+            server_default=sa.text("'{}'"),
+        ),
+        sa.Column("created_at", sa.TIMESTAMP(), nullable=False),
+        sa.Column("updated_at", sa.TIMESTAMP(), nullable=False),
+        sa.Column("row_status", sa.String(), nullable=False),
+        sa.Column("performed_by", sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
 
 def downgrade() -> None:
     op.drop_table("users")
@@ -103,3 +150,5 @@ def downgrade() -> None:
     op.drop_table("products")
     op.drop_table("merchant")
     op.drop_table("merchant_product")
+    op.drop_table("order")
+    op.drop_table("order_lines")
